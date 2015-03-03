1bbfa29984019b480ca03292c169c6ad62b929d3
ArchonWing <nberger21@gmail.com>
2015-03-03 12:30:50 -0500

    Added Ship class and began adding in stub messages.

--- Battleship/src/Battleship.java
+++ Battleship/src/Battleship.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+
 
 public class Battleship {
 
-	public void rungame() {
+	//array list to hold ships
+	ArrayList<Ship> ships = new ArrayList<Ship>();
+	
+	
+	//method to setup new game
+	public void setUpGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//method to run game
+	public void runGame() {
 		// TODO Auto-generated method stub
 		
 	}
--- Battleship/src/BattleshipLauncher.java
+++ Battleship/src/BattleshipLauncher.java
@@ -2,10 +2,15 @@
 public class BattleshipLauncher {
 
 	public static void main(String[] args) {
-		
+
+		// create new battleship game
 		Battleship g = new Battleship();
+
+		//setup battleship game
+		g.setUpGame();
 		
-		g.rungame();
+		// run battleship game
+		g.runGame();
 
 	}
 
